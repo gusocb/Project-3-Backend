99f79d806a8f09ed3b96288b77e6bd69ca2e8880
Gustavo Cruz <gustavocruzb@outlook.com>
2020-04-14 20:02:48 -0500

    stock update working

--- routes/sales-routes.js
+++ routes/sales-routes.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 let Sale = require('../models/Sale')
+let Product = require('../models/Product')
 
 //Add sale
-router.post('/checkout',(req,res) => {
-    
+router.post('/checkout', (req,res) => {
+
     Sale.create({ 
         sale:req.body.sale ,
         total:req.body.total,
         owner:req.body.owner
     })
-      .then(() => res.json('sale added'))
-      .catch(err => console.log(err))
+    .then(()=> res.json('sale added'))
+    .catch(err => console.log(err))
     
+    req.body.sale.forEach( async ele => {
+        await Product.findByIdAndUpdate(ele._id,{stock:ele.newStock})
+    })
 });
 
 module.exports = router;
+
